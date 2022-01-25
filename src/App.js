1c21226867047336e099eee33063ae3a5718807b
chrisrw300 <chrisrw300@gmail.com>
2022-01-24 21:31:11 -0700

    added more styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import './App.css';
-import Nav from './Components/Nav'
-import Forecast from './Components/Forecast';
 import Dashboard from './Components/Dashboard'
-import Daily from './Components/Daily'
 
 function App() {
   return (
     <div className="App">
-      <Nav />
       <Dashboard />
     </div>
   );
--- src/Components/Dashboard/index.js
+++ src/Components/Dashboard/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import Forecast from '../Forecast';
+import Daily from '../Daily'
 
 function Dashboard() {
     return (
         <section>
-            <div class="search">
-                <input type="text" placeholder="Albuquerque, etc..." id="search-input"></input>
-                <button type="button" id="search-btn">Search</button>
+            <div class="body">
+                <div class="search">
+                    <input type="text" placeholder="Albuquerque, etc..." id="search-input"></input>
+                    <button type="button" id="search-btn">Search</button>
+                </div>
             </div>
+            <Forecast />
+            <Daily />
         </section>
     )
 }
